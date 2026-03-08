3d6972495550778a60bb670a9c40c5bba64fb45b
MAC OS <macos@MACs-MacBook-Air.local>
2026-03-08 18:16:37 +0700

    fix mlflow sqlite tracking

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,8 +6,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-# gunakan local mlflow tracking (CI friendly)
-mlflow.set_tracking_uri("file:./mlruns")
+# gunakan SQLite tracking (CI friendly)
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+
+# set experiment
+mlflow.set_experiment("bank-marketing-model")
 
 # pastikan tidak ada run lama
 mlflow.end_run()
@@ -15,7 +18,7 @@ mlflow.end_run()
 # load dataset
 data = pd.read_csv("dataset_preprocessing/bank_marketing_clean.csv")
 
-# fitur dan target
+# split feature dan target
 X = data.drop("deposit", axis=1)
 y = data["deposit"]
 
@@ -34,28 +37,22 @@ with mlflow.start_run():
         random_state=42
     )
 
-    # log parameter
     mlflow.log_param("n_estimators", 100)
 
-    # training
     model.fit(X_train, y_train)
 
-    # prediksi
     y_pred = model.predict(X_test)
 
-    # evaluasi
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred, average="binary")
     recall = recall_score(y_test, y_pred, average="binary")
     f1 = f1_score(y_test, y_pred, average="binary")
 
-    # log metrics
     mlflow.log_metric("accuracy", accuracy)
     mlflow.log_metric("precision", precision)
     mlflow.log_metric("recall", recall)
     mlflow.log_metric("f1_score", f1)
 
-    # log model
     mlflow.sklearn.log_model(model, "model")
 
 print("Training selesai")
