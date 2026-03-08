c9ec17ae8039a38a9231587d360c0818b607521c
MAC OS <macos@MACs-MacBook-Air.local>
2026-03-08 17:56:16 +0700

    fix mlflow run for CI svaha astungkara bisa

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -2,22 +2,18 @@ import pandas as pd
 import mlflow
 import mlflow.sklearn
 
-mlflow.set_tracking_uri("file:./mlruns")
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-mlflow.sklearn.autolog()
-
+mlflow.set_tracking_uri("file:./mlruns")
 data = pd.read_csv("dataset_preprocessing/bank_marketing_clean.csv")
 
 X = data.drop("deposit", axis=1)
 y = data["deposit"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
+    X, y,
     test_size=0.2,
     random_state=42
 )
@@ -26,7 +22,6 @@ with mlflow.start_run():
 
     model = RandomForestClassifier(
         n_estimators=100,
-        max_depth=10,
         random_state=42
     )
 
@@ -39,14 +34,11 @@ with mlflow.start_run():
     recall = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
 
-    print("Accuracy:", accuracy)
-    print("Precision:", precision)
-    print("Recall:", recall)
-    print("F1:", f1)
-
     mlflow.log_metric("accuracy", accuracy)
     mlflow.log_metric("precision", precision)
     mlflow.log_metric("recall", recall)
-    mlflow.log_metric("f1_score", f1)
+    mlflow.log_metric("f1", f1)
 
     mlflow.sklearn.log_model(model, "model")
+
+print("Training selesai")
