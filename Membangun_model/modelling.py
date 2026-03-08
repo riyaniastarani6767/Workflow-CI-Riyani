912924917b15813fa8fe03265295664935b70dd0
MAC OS <macos@MACs-MacBook-Air.local>
2026-03-08 17:19:32 +0700

    fix dataset path for CI

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -1,44 +1,39 @@
-
 import pandas as pd
 import mlflow
 import mlflow.sklearn
+
 mlflow.set_tracking_uri("http://127.0.0.1:5000")
 
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-# aktifkan autolog
 mlflow.sklearn.autolog()
 
-# load dataset
-data = pd.read_csv("dataset_preprocessing/bank_marketing_clean.csv")
+data = pd.read_csv("MLProject/dataset_preprocessing/bank_marketing_clean.csv")
 
-# fitur dan target
 X = data.drop("deposit", axis=1)
 y = data["deposit"]
 
-# split data
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
 )
 
 with mlflow.start_run():
 
-    # model
     model = RandomForestClassifier(
         n_estimators=100,
         max_depth=10,
         random_state=42
     )
 
-    # training
     model.fit(X_train, y_train)
 
-    # prediksi
     y_pred = model.predict(X_test)
 
-    # hitung metric
     accuracy = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
@@ -49,11 +44,9 @@ with mlflow.start_run():
     print("Recall:", recall)
     print("F1:", f1)
 
-    # log metrics manual
     mlflow.log_metric("accuracy", accuracy)
     mlflow.log_metric("precision", precision)
     mlflow.log_metric("recall", recall)
     mlflow.log_metric("f1_score", f1)
 
-    # log model manual
     mlflow.sklearn.log_model(model, "model")
