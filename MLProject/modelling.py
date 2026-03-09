45da2d4d45fe431bb9bd8738f59f96998a32aed7
MAC OS <macos@MACs-MacBook-Air.local>
2026-03-09 11:07:34 +0700

    astungkara bisa ya

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -3,8 +3,9 @@ import mlflow
 import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
+mlflow.end_run()
 mlflow.set_tracking_uri("sqlite:///mlruns.db")
 mlflow.set_experiment("bank-marketing-model")
 
@@ -17,8 +18,6 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-mlflow.sklearn.autolog()
-
 with mlflow.start_run():
     model = RandomForestClassifier(
         n_estimators=100,
@@ -27,6 +26,13 @@ with mlflow.start_run():
     )
     model.fit(X_train, y_train)
     y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    mlflow.log_metric("accuracy", acc)
+
+    mlflow.log_param("n_estimators", 100)
+    mlflow.log_param("max_depth", 10)
+    mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+    mlflow.log_metric("precision", precision_score(y_test, y_pred, average="binary"))
+    mlflow.log_metric("recall", recall_score(y_test, y_pred, average="binary"))
+    mlflow.log_metric("f1_score", f1_score(y_test, y_pred, average="binary"))
     mlflow.sklearn.log_model(model, "model")
+
+print("Training selesai")
