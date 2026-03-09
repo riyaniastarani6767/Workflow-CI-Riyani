9092a1baea5d01c95d9ed2df71b2e2e0631236d5
MAC OS <macos@MACs-MacBook-Air.local>
2026-03-09 18:37:28 +0700

    3

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,12 +1,55 @@
+# import pandas as pd
+# import mlflow
+# import mlflow.sklearn
+
+# from sklearn.model_selection import train_test_split
+# from sklearn.ensemble import RandomForestClassifier
+# from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+# mlflow.autolog()
+
+# data = pd.read_csv("dataset_preprocessing/bank_marketing_clean.csv")
+
+# X = data.drop("deposit", axis=1)
+# y = data["deposit"]
+
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X,
+#     y,
+#     test_size=0.2,
+#     random_state=42
+# )
+
+# model = RandomForestClassifier(
+#     n_estimators=100,
+#     max_depth=10,
+#     random_state=42
+# )
+
+# model.fit(X_train, y_train)
+
+# y_pred = model.predict(X_test)
+
+# accuracy = accuracy_score(y_test, y_pred)
+# precision = precision_score(y_test, y_pred)
+# recall = recall_score(y_test, y_pred)
+# f1 = f1_score(y_test, y_pred)
+
+# print("Accuracy:", accuracy)
+# print("Precision:", precision)
+# print("Recall:", recall)
+# print("F1 Score:", f1)
+
+# print("Training selesai")
+
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 
-mlflow.autolog()
+mlflow.sklearn.autolog()
 
 data = pd.read_csv("dataset_preprocessing/bank_marketing_clean.csv")
 
@@ -14,30 +57,21 @@ X = data.drop("deposit", axis=1)
 y = data["deposit"]
 
 X_train, X_test, y_train, y_test = train_test_split(
-    X,
-    y,
-    test_size=0.2,
-    random_state=42
+    X, y, test_size=0.2, random_state=42
 )
 
-model = RandomForestClassifier(
-    n_estimators=100,
-    max_depth=10,
-    random_state=42
-)
+with mlflow.start_run():
+    model = RandomForestClassifier(
+        n_estimators=100,
+        max_depth=10,
+        random_state=42
+    )
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
 
-model.fit(X_train, y_train)
-
-y_pred = model.predict(X_test)
-
-accuracy = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
-
-print("Accuracy:", accuracy)
-print("Precision:", precision)
-print("Recall:", recall)
-print("F1 Score:", f1)
+    mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
+    mlflow.log_metric("precision", precision_score(y_test, y_pred))
+    mlflow.log_metric("recall", recall_score(y_test, y_pred))
+    mlflow.log_metric("f1_score", f1_score(y_test, y_pred))
 
 print("Training selesai")
